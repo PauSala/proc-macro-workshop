d644ef130484ee1ea3b707dce930be9a95a31b2c
pausala <pau.salaf@gmail.com>
2024-10-26 16:38:13 +0200

    solutions for debug and builder exercises

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,216 @@
+#![recursion_limit = "128"]
+
+extern crate proc_macro;
+
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
-#[proc_macro_derive(Builder)]
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = parse_macro_input!(input as DeriveInput);
+    let name = &ast.ident;
+    let bname = format!("{}Builder", name);
+    let bident = syn::Ident::new(&bname, name.span());
+    let fields = if let syn::Data::Struct(syn::DataStruct {
+        fields: syn::Fields::Named(syn::FieldsNamed { ref named, .. }),
+        ..
+    }) = ast.data
+    {
+        named
+    } else {
+        unimplemented!();
+    };
 
-    unimplemented!()
+    let builder_fields = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        if ty_inner_type("Option", ty).is_some() || builder_of(&f).is_some() {
+            quote! { #name: #ty }
+        } else {
+            quote! { #name: std::option::Option<#ty> }
+        }
+    });
+
+    let methods = fields.iter().map(|f| {
+        let name = f.ident.as_ref().unwrap();
+        let ty = &f.ty;
+
+        let (arg_type, value) = if let Some(inner_ty) = ty_inner_type("Option", ty) {
+            // if the field is an Option<T>, setting should take just a T,
+            // but we then need to store it within a Some.
+            (inner_ty, quote! { std::option::Option::Some(#name) })
+        } else if builder_of(&f).is_some() {
+            // if the field is a builder, it is a Vec<T>,
+            // and the value in the builder is _not_ wrapped in an Option,
+            // so we shouldn't wrap the value in Some.
+            (ty, quote! { #name })
+        } else {
+            // otherwise, we take the type used by the target,
+            // and we store it in an Option in the builder
+            // in case it was never set.
+            (ty, quote! { std::option::Option::Some(#name) })
+        };
+        let set_method = quote! {
+            pub fn #name(&mut self, #name: #arg_type) -> &mut Self {
+                self.#name = #value;
+                self
+            }
+        };
+
+        // we need to take care not to include a builder method with the same name as the set
+        // method. for example, consider this struct:
+        //
+        // ```
+        // #[derive(Builder)]
+        // struct Command {
+        //     #[builder(each = "env")]
+        //     env: Vec<String>
+        // }
+        // ```
+        //
+        // It would not be okay to generate both `env(Vec<String>)` for the field
+        // *and* `env(String)` for the builder.
+        match extend_method(&f) {
+            None => set_method.into(),
+            Some((true, extend_method)) => extend_method,
+            Some((false, extend_method)) => {
+                // safe to generate both!
+                let expr = quote! {
+                    #set_method
+                    #extend_method
+                };
+                expr.into()
+            }
+        }
+    });
+
+    // for when you call Builder::build
+    let build_fields = fields.iter().map(|f| {
+        let name = &f.ident;
+        if ty_inner_type("Option", &f.ty).is_some() || builder_of(f).is_some() {
+            quote! {
+                #name: self.#name.clone()
+            }
+        } else {
+            quote! {
+                #name: self.#name.clone().ok_or(concat!(stringify!(#name), " is not set"))?
+            }
+        }
+    });
+
+    let build_empty = fields.iter().map(|f| {
+        let name = &f.ident;
+        if builder_of(f).is_some() {
+            quote! { #name: std::vec::Vec::new() }
+        } else {
+            quote! { #name: std::option::Option::None }
+        }
+    });
+
+    let doc = format!("\
+            Implements the [builder pattern] for [`{}`].\n\
+            \n\
+            [builder pattern]: https://rust-lang-nursery.github.io/api-guidelines/type-safety.html#c-builder", name);
+
+    let expanded = quote! {
+        #[doc = #doc]
+        pub struct #bident {
+            #(#builder_fields,)*
+        }
+        impl #bident {
+            #(#methods)*
+
+            pub fn build(&self) -> std::result::Result<#name, std::boxed::Box<dyn std::error::Error>> {
+                std::result::Result::Ok(#name {
+                    #(#build_fields,)*
+                })
+            }
+        }
+        impl #name {
+            pub fn builder() -> #bident {
+                #bident {
+                    #(#build_empty,)*
+                }
+            }
+        }
+    };
+
+    expanded.into()
+}
+
+fn builder_of(f: &syn::Field) -> Option<&syn::Attribute> {
+    for attr in &f.attrs {
+        if attr.path().segments.len() == 1 && attr.path().segments[0].ident == "builder" {
+            return Some(attr);
+        }
+    }
+    None
+}
+
+fn extend_method(f: &syn::Field) -> Option<(bool, proc_macro2::TokenStream)> {
+    let name = f.ident.as_ref().unwrap();
+    let g = builder_of(f)?;
+
+    fn mk_err<T: quote::ToTokens>(t: T) -> Option<(bool, proc_macro2::TokenStream)> {
+        Some((
+            false,
+            syn::Error::new_spanned(t, "expected `builder(each = \"...\")`").to_compile_error(),
+        ))
+    }
+
+    let meta = match g.parse_args::<syn::Meta>() {
+        Ok(syn::Meta::NameValue(nv)) => {
+            if nv.path.get_ident().expect("An ident should exist") != "each" {
+                return mk_err(&g.meta);
+            }
+            nv.value
+        }
+        Ok(meta) => {
+            // inside of #[] there was either just an identifier (`#[builder]`) or a key-value
+            // mapping (`#[builder = "foo"]`), neither of which are okay.
+            return mk_err(meta);
+        }
+        Err(e) => {
+            return Some((false, e.to_compile_error()));
+        }
+    };
+
+    match meta {
+        syn::Expr::Lit(exp_lit) => match exp_lit.lit {
+            syn::Lit::Str(s) => {
+                let arg = syn::Ident::new(&s.value(), s.span());
+                let inner_ty = ty_inner_type("Vec", &f.ty).unwrap();
+                let method = quote! {
+                    pub fn #arg(&mut self, #arg: #inner_ty) -> &mut Self {
+                        self.#name.push(#arg);
+                        self
+                    }
+                };
+                return Some((&arg == name, method));
+            }
+            _ => panic!("expected string, found {:?}", exp_lit),
+        },
+        _ => return mk_err(meta),
+    }
+}
+
+fn ty_inner_type<'a>(wrapper: &str, ty: &'a syn::Type) -> Option<&'a syn::Type> {
+    if let syn::Type::Path(ref p) = ty {
+        if p.path.segments.len() != 1 || p.path.segments[0].ident != wrapper {
+            return None;
+        }
+
+        if let syn::PathArguments::AngleBracketed(ref inner_ty) = p.path.segments[0].arguments {
+            if inner_ty.args.len() != 1 {
+                return None;
+            }
+
+            let inner_ty = inner_ty.args.first().unwrap();
+            if let syn::GenericArgument::Type(ref t) = inner_ty {
+                return Some(t);
+            }
+        }
+    }
+    None
 }
--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,305 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{
+    parse_macro_input, parse_quote, punctuated::Punctuated, token::Comma, DeriveInput, Field,
+    GenericParam, Generics, PathArguments, PathSegment,
+};
 
-#[proc_macro_derive(CustomDebug)]
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = parse_macro_input!(input as DeriveInput);
+    let name = &ast.ident;
 
-    unimplemented!()
+    let struct_name = format!("{}", name);
+    let fields = if let syn::Data::Struct(syn::DataStruct {
+        fields: syn::Fields::Named(syn::FieldsNamed { ref named, .. }),
+        ..
+    }) = ast.data
+    {
+        named
+    } else {
+        // TODO: return appropiate compile error
+        unimplemented!();
+    };
+
+    // Heuristic 1:
+    // Generics in PhantomData are omited (test case 4);
+    // So find them to filter out.
+    let mut omits = identify_phantom_data_generics(&fields);
+
+    // Heuristic 2:
+    // Try to force associated types of traits to implement debug via where clauses.
+    let where_clauses = build_where_clause_for_associated_types(&ast.generics, fields);
+    // Since they appear in the where clause, corresponding generic idents must be ommited as well from where clauses.
+    for clause in &where_clauses {
+        omits.push(clause.1.clone());
+    }
+
+    // Provide an escape hatch for cases where it is not possible to apply an heuristic.
+    // This hatch should be a macro atribute like: `#[debug(bound = "T::Value: Debug")]`
+    let mut attrs = ast.attrs;
+    let mut escape_hatch = (false, quote! {});
+    if let Some(attr) = attrs.pop() {
+        let literal = get_attrs_literal(attr);
+        match literal {
+            Err(e) => return e.to_compile_error().into(),
+            Ok(literal) => {
+                let parsed = syn::parse_str::<syn::WherePredicate>(&literal).unwrap();
+                escape_hatch = (
+                    true,
+                    quote! {
+                        where #parsed
+                    },
+                )
+            }
+        }
+    }
+
+    // Generic types bounds
+    // If an escape hatch is provided, no bounds are set.
+    // As the last test suggests, it would be an improvement to provide the hatch at attribute level too.
+    let mut generics = ast.generics;
+    if !escape_hatch.0 {
+        generics = add_trait_bounds(generics, &omits);
+    }
+    let (impl_generics, ty_generics, _) = generics.split_for_impl();
+
+    // Prepare where clauses.
+    // This takes in account the heuristic 2 (associated types for traits) and the scape hatch, giving the latest most priority.
+    let mut wc = quote! {};
+    let where_clauses: Vec<_> = where_clauses.iter().map(|c| c.0.clone()).collect();
+    if escape_hatch.0 {
+        wc = escape_hatch.1;
+    } else if !where_clauses.is_empty() {
+        wc = quote! {
+            where
+                #(#where_clauses)*
+        };
+    }
+
+    // Get calls to the fields in the debug_struct function chain
+    let debug_calls = debug_calls(&fields);
+    let debug_calls = match debug_calls {
+        Ok(calls) => calls,
+        Err(e) => return e.to_compile_error().into(),
+    };
+
+    quote! {
+        impl #impl_generics std::fmt::Debug for #name #ty_generics #wc  {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                f.debug_struct(#struct_name)
+                 #(#debug_calls)*
+                 .finish()
+            }
+        }
+    }
+    .into()
+}
+
+/// Get calls to the fields in the debug_struct function chain
+fn debug_calls(
+    fields: &Punctuated<Field, Comma>,
+) -> Result<Vec<proc_macro2::TokenStream>, syn::Error> {
+    let debug_calls: Result<Vec<_>, syn::Error> = fields
+        .iter()
+        .map(|f| {
+            let ident = &f.ident;
+            let name = format!("{}", &ident.as_ref().unwrap());
+            let attrs = &f.attrs;
+
+            // If the field has an atribute of the form #[debug = [format]]
+            if attrs.len() > 0 {
+                let attr = &attrs[0];
+                return attr_debug_call(attr, name, ident);
+            } else {
+                Ok(quote! {
+                    .field(#name, &self.#ident)
+                })
+            }
+        })
+        .collect();
+    debug_calls
+}
+
+/// Get debug call for a field with a formatter atribute.
+///
+/// For example in the bitmask field
+///
+/// ```
+/// use derive_debug::CustomDebug;
+/// #[derive(CustomDebug)]
+/// pub struct Field {
+///     name: &'static str,
+///     #[debug = "0b{:08b}"]
+///     bitmask: u8,
+/// }
+///
+fn attr_debug_call(
+    attr: &syn::Attribute,
+    name: String,
+    ident: &Option<syn::Ident>,
+) -> Result<proc_macro2::TokenStream, syn::Error> {
+    let (_, value) = match &attr.meta.require_name_value() {
+        Ok(n) => match &n.value {
+            syn::Expr::Lit(syn::ExprLit {
+                lit: syn::Lit::Str(ref lit_str),
+                ..
+            }) => (n.path.get_ident().unwrap(), lit_str.value()),
+            _ => {
+                return Err(syn::Error::new_spanned(
+                    &n.value,
+                    format!("Expected a string formater, for example: {}", {
+                        "\"0b{:08b}\""
+                    }),
+                ));
+            }
+        },
+        Err(_) => {
+            return Err(syn::Error::new_spanned(attr, "Invalid attribute format"));
+        }
+    };
+    Ok(quote! {
+        .field(#name, &format_args!(#value, &self.#ident))
+    })
+}
+
+/// Extracts the attribute's literal in the escape hatch attribute.
+fn get_attrs_literal(attr: syn::Attribute) -> Result<String, syn::Error> {
+    let list = attr.meta.require_list();
+    match list {
+        Err(_) => return Err(syn::Error::new_spanned(attr, "Invalid argument")),
+        Ok(l) => {
+            let mut tokens = l.tokens.clone().into_iter();
+
+            // Check that the first token is an Ident with the value "bound"
+            if let Some(proc_macro2::TokenTree::Ident(ident)) = tokens.next() {
+                if ident != "bound" {
+                    return Err(syn::Error::new_spanned(ident, "Invalid ident"));
+                }
+            } else {
+                return Err(syn::Error::new_spanned(&l, "Invalid argument"));
+            }
+
+            // Check that the second token is a Punct with the character '='
+            if let Some(proc_macro2::TokenTree::Punct(punct)) = tokens.next() {
+                if punct.as_char() != '=' {
+                    return Err(syn::Error::new_spanned(
+                        punct,
+                        "Invalid punct, it should be '='",
+                    ));
+                }
+            } else {
+                return Err(syn::Error::new_spanned(&l, "Invalid argument"));
+            }
+
+            // Check that the third token is a Literal and extract its value
+            if let Some(proc_macro2::TokenTree::Literal(literal)) = tokens.next() {
+                let stripped_literal = literal.to_string().trim_matches('"').to_owned();
+                return Ok(stripped_literal);
+            } else {
+                return Err(syn::Error::new_spanned(&l, "Invalid argument"));
+            }
+        }
+    }
+}
+
+fn build_where_clause_for_associated_types(
+    generics: &Generics,
+    fields: &Punctuated<Field, Comma>,
+) -> Vec<(proc_macro2::TokenStream, syn::Ident)> {
+    let mut generic_params_idents = Vec::new();
+    for g in &generics.params {
+        if let syn::GenericParam::Type(syn::TypeParam { ref ident, .. }) = g {
+            generic_params_idents.push(ident);
+        }
+    }
+    if generic_params_idents.is_empty() {
+        return vec![];
+    }
+    let mut res = vec![];
+    let generic_strings = &generic_params_idents
+        .iter()
+        .map(|e| e.to_string())
+        .collect();
+    for f in fields {
+        if let syn::Type::Path(syn::TypePath { ref path, .. }) = f.ty {
+            recurse_type_path(&path, &mut res, &generic_strings);
+        }
+    }
+
+    return res;
+}
+
+fn recurse_type_path(
+    path: &syn::Path,
+    res: &mut Vec<(proc_macro2::TokenStream, syn::Ident)>,
+    generics: &Vec<String>,
+) {
+    if let Some(segment) = path.segments.first() {
+        if generics.contains(&segment.ident.to_string()) && path.segments.len() > 1 {
+            res.push((
+                quote! {
+                    #path: std::fmt::Debug,
+                },
+                segment.ident.clone(),
+            ));
+        } else if let PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments {
+            ref args,
+            ..
+        }) = &segment.arguments
+        {
+            for arg in args {
+                if let syn::GenericArgument::Type(syn::Type::Path(syn::TypePath { path, .. })) = arg
+                {
+                    recurse_type_path(path, res, generics);
+                }
+            }
+        }
+    }
+}
+
+fn identify_phantom_data_generics(fields: &Punctuated<Field, Comma>) -> Vec<syn::Ident> {
+    let mut res = vec![];
+    for field in fields {
+        if let syn::Type::Path(ref p) = field.ty {
+            for segment in &p.path.segments {
+                if segment.ident == "PhantomData" {
+                    let gen = find_generic_idents(&segment);
+                    for i in gen {
+                        res.push(i.clone());
+                    }
+                }
+            }
+        }
+    }
+    res
+}
+
+fn find_generic_idents<'a>(segment: &'a PathSegment) -> Vec<&'a syn::Ident> {
+    let mut res = vec![];
+    if let syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments {
+        args, ..
+    }) = &segment.arguments
+    {
+        for arg in args {
+            if let syn::GenericArgument::Type(syn::Type::Path(ref segments, ..)) = arg {
+                for s in &segments.path.segments {
+                    res.push(&s.ident);
+                }
+            }
+        }
+    }
+    res
+}
+
+/// Add trait bound Debug for types not in omits
+fn add_trait_bounds(mut generics: Generics, omits: &Vec<syn::Ident>) -> Generics {
+    for param in &mut generics.params {
+        if let GenericParam::Type(ref mut type_param) = *param {
+            if !omits.contains(&type_param.ident) {
+                type_param.bounds.push(parse_quote!(std::fmt::Debug));
+            }
+        }
+    }
+    generics
 }
